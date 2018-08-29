f1b5623a68d4a21fe56ce898cfd9d696505b4584
Niewenqiang <37056602+Niewenqiang@users.noreply.github.com>
2018-08-29 10:46:51 +0800

    孩子们的游戏（圆圈中最后剩下的数）

---
+++ LastRemaining.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+# 问题描述：n个人（编号0~(n-1))，从0开始报数，报到(m-1)的退出，剩下的人继续从0开始报数。求胜利者的编号。
+
+
+class Solution:
+    def LastRemaining_Solution(self, n, m):
+        if n < 1:
+            return -1
+        res, i = 0, 2
+        while i <= n:
+            res = (res + m) % i
+            i += 1
+        return res
+
+
+s = Solution()
+print s.LastRemaining_Solution(13, 4)
