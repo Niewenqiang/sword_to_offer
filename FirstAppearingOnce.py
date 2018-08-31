469da9f60e8cc0a4fdbac50aadf21bd7fc2eb3f1
Niewenqiang <37056602+Niewenqiang@users.noreply.github.com>
2018-08-31 13:18:11 +0800

    字符流中第一个不重复的字符

---
+++ FirstAppearingOnce.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+# 字符流中第一个不重复的字符
+# 请实现一个函数用来找出字符流中第一个只出现一次的字符。例如，当从字符流中只读出前两个字符"go"时，
+# 第一个只出现一次的字符是"g"。当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+#
+# 输出描述：
+#     如果当前字符流没有存在出现一次的字符，返回#字符。
+
+# 1、思路
+# 这道题还是很简单的。将字节流保存起来，通过哈希表统计字符流中每个字符出现的次数，顺便将字符流保存在string中，
+# 然后再遍历string，从哈希表中找到第一个出现一次的字符。
+
+
+class Solution:
+    def FirstAppearingOnce(self, s):
+        if s is []:
+            return '#'
+        i = 0
+        while i < len(s):
+            if s[i] not in s[:i] and s[i] not in s[i+1:]:
+                return s[i]
+            else:
+                i += 1
+        return '#'
+
+
+s = Solution()
+print s.FirstAppearingOnce('google')
