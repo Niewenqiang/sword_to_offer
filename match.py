5064c54bc59ef0f02abcec8f4f97581d78219c15
Niewenqiang <37056602+Niewenqiang@users.noreply.github.com>
2018-09-01 22:11:19 +0800

    正则表达式匹配

---
+++ match.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+# 正则表达式匹配
+
+# 请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（包含0次）。
+# 在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配。
+
+
+class Solution:
+    def match(self, s, pattern):
+        if s == pattern:
+            return True
+        if len(pattern) > 1 and pattern[1] == '*':
+            if s and (s[0] == pattern[0] or pattern[0] == '.'):
+                return self.match(s, pattern[2:]) or self.match(s[1:], pattern)
+            else:
+                return self.match(s, pattern[2:])
+        elif s and pattern and (s[0] == pattern[0] or pattern[0] == '.'):
+            return self.match(s[1:], pattern[1:])
+        return False
+
+
+s = Solution()
+print s.match('aaa', 'ab*ac*a')
+print s.match('aaa', 'a.a')
