fc62cfe692960650f6e80f91cdbe455a3634b4f6
Niewenqiang <37056602+Niewenqiang@users.noreply.github.com>
2021-05-27 14:17:16 +0800

    数字在排序数组中出现的次数

--- GetNumberOfK.py
+++ GetNumberOfK.py
@@ -11,3 +11,29 @@ class Solution:
 
 s = Solution()
 print s.GetNumberOfK([1, 2, 3, 4, 5, 3], 3)
+
+
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetNumberOfK(self, data, k):
+        if len(data) < 1:
+            return 0
+        mid = len(data)//2
+        if data[mid] == k:
+            start,end =mid,mid
+            for i in range(mid-1,-1,-1):
+                if data[i] == k:
+                    start = i
+            for i in range(mid+1,len(data)):
+                if data[i] == k:
+                    end = i
+            return end-start+1
+        elif data[mid] < k:
+            return self.GetNumberOfK(data[mid+1:],k)
+        else:
+            return self.GetNumberOfK(data[:mid],k)
+        
+        
+        
+        
